872ddb62f6b76a7c97000861f2e6b7b9328b8842
Ahmed Fasih <wuzzyview@gmail.com>
2015-09-22 11:32:50 -0400

    Clip object successfully built

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ var button =
                 selection : selection
               };
               console.log(obj);
+
+              var req = new XMLHttpRequest();
+              req.onreadystatechange = (() => {
+                console.log(req.readyState || 'no readyState',
+                            req.status || 'no status',
+                            req.responseText || 'no responseText');
+              });
+              req.open('POST', 'https://127.0.0.1:4001/clip', true);
+              req.setRequestHeader('Content-Type', 'application/json');
+              req.withCredentials = true;
+              req.send(JSON.stringify(obj));
+
             });
           }
         });
